cf6cb0e485aed6d453773220503c116346ee0188
Sathish1504 <sathishkumar150401@gmail.com>
2026-07-18 13:03:45 +0530

    feat(chat): add core chat domain entities

---
+++ services/ChatService/src/Chat.Domain/Entities/Conversation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Domain.Entities
+{
+    internal class Conversation
+    {
+    }
+}
---
+++ services/ChatService/src/Chat.Domain/Enums/ConversationType.cs
@@ -0,0 +1,7 @@
+﻿namespace Chat.Domain.Enums;
+
+public enum ConversationType
+{
+    Direct = 1,
+    Group = 2
+}
